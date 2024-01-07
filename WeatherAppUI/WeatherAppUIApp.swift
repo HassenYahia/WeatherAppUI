93c772ea3f465e928ca7e6b2fae82f3697de3ffb
HassenYahia <hassenabdurahman4047@gmail.com>
2024-01-06 21:11:30 -0500

    Initial Commit

---
+++ WeatherAppUI/WeatherAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppUIApp.swift
+//  WeatherAppUI
+//
+//  Created by Hassen Abdurahman Yahia on 2024-01-06.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
